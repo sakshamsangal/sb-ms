5969fe21e3a054fcfe6cccf3ff5faf076f38b598
Saksham Sangal <sakshamsangal111@gmail.com>
2023-01-28 00:22:51 +0530

    up

---
+++ api-gateway/src/main/java/com/example/apigateway/CorsConfiguration.java
@@ -0,0 +1,13 @@
+//package com.example.apigateway;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.reactive.CorsWebFilter;
+//
+//import java.util.Arrays;
+//import java.util.Collections;
+//
+//@Configuration
+//public class CorsConfiguration {
+//
+//}
